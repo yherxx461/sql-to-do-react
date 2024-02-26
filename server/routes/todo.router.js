9896487ae969bb8e42697c77c1f1ab998ddb319c
Ying Her <yher.prime@gmail.com>
2024-02-25 22:17:14 -0600

    GET Route working in Postman

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,7 +3,19 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
+router.get('/', (req, res) => {
+  const dbQuery = `SELECT * FROM "todo" ORDER BY "id";`;
 
+  pool
+    .query(dbQuery)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log('ERROR in getting the "todo" list from the database', error);
+      res.sendStatus(500);
+    });
+});
 // POST
 
 // PUT
