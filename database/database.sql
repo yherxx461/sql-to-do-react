e0696aae4b48d3ba17bb9da748b831f7bc423270
Ying Her <yher.prime@gmail.com>
2024-02-25 22:02:59 -0600

    database.sql created with table name 'to-do'

---
+++ database/database.sql
@@ -0,0 +1,16 @@
+CREATE TABLE "to-do" (
+  "id" SERIAL PRIMARY KEY,
+  "task" VARCHAR(120) NOT NULL,
+  "completed" BOOLEAN DEFAULT FALSE
+);
+
+INSERT INTO "to-do" ("task")
+VALUES ('Make breakfast'),
+	('Group work'),
+	('Grocery Shopping'),
+	('Do laundry'
+);
+
+SELECT * FROM "todo";
+
+DROP TABLE "todo";
