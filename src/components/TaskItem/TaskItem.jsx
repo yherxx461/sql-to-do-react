caa9aee55d514300f66da04208f411532e97ce5b
Ying Her <yher.prime@gmail.com>
2024-03-06 06:59:48 -0600

    TasksList component created to call all tasks

---
+++ src/components/TaskItem/TaskItem.jsx
@@ -0,0 +1,12 @@
+import React, {useEffect, useState} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+function TaskItem() {
+  const [newTask, setNewTask] = useState({
+    name: '',
+    completed: '',
+  });
+  const 
+}
+
+export default TaskItem;
