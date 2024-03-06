da74457fcf8338889a75dae2f68d55b1467b9998
Ying Her <yher.prime@gmail.com>
2024-03-06 07:05:47 -0600

    imported TaskList to the App component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react';
 import Form from '../Form/Form';
+import TasksList from '../TasksList/TasksList';
 
 function App() {
   return (
     <div>
       <h1>TO DO APP</h1>
       <Form />
+      <TasksList />
     </div>
   );
 }
