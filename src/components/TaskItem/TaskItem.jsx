a7b6b221a4295ceab8ff4b6c54b9d4df3044ff0e
Ying Her <yher.prime@gmail.com>
2024-04-21 21:10:28 -0500

    added handleDelete in TasksList and make sure POST request is working and can add new task to the client and backend

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -1,26 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+// import React, { useEffect, useState } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
 
-function TaskItem() {
-  const [newTask, setNewTask] = useState({
-    task: '',
-    completed: '',
-  });
-  const task = useSelector((state) => state.addTask);
-  const dispatch = useDispatch();
+// function TaskItem() {
+//   const [newTask, setNewTask] = useState({
+//     task: '',
+//     completed: '',
+//   });
+//   const task = useSelector((state) => state.addTask);
+//   const dispatch = useDispatch();
 
-  // Initialized the first load
-  useEffect(() => {
-    dispatch({ type: 'ADD_NEW_TASK' });
-  }, []);
+//   // Initialized the first load
+//   useEffect(() => {
+//     dispatch({ type: 'ADD_NEW_TASK' });
+//   }, []);
 
-  return (
-    <div className="task-item">
-      <p>
-        {newTask.task} {newTask.completed}
-      </p>
-    </div>
-  );
-}
+//   return (
+//     <div className="task-item">
+//       <p></p>
+//     </div>
+//   );
+// }
 
-export default TaskItem;
+// export default TaskItem;
--- src/components/TasksList/TasksList.jsx
+++ src/components/TasksList/TasksList.jsx
@@ -1,23 +1,50 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+
+// const [taskToggled, setTaskToggled] = useState(false);
 
 function TasksList() {
   const dispatch = useDispatch();
   const tasks = useSelector((store) => store.tasks);
+  const deleteTask = useSelector((store) => store.deleteTask);
+  const [task, setTask] = useState(tasks.task);
+  const [completed, setCompleted] = useState(tasks.completed);
 
   useEffect(() => {
     dispatch({
       type: 'FETCH_ALL_TASKS',
     });
-  }, []);
+  }, [dispatch]);
+
+  // const handleClickToggle = (taskData) => {
+  //   const toggleTask = (taskData) => {
+  //     console.log('in handleClickToggle() - taskData:', taskToggled);
+  //     setTask;
+  //   };
+  // };
+
+  const handleDelete = (taskId) => {
+    // console.log('in handleDelete');
+    dispatch({
+      type: 'DELETE_TASK',
+      payload: { id: taskId },
+    });
+  };
 
   return (
     <main>
       <section className="tasks">
-        {tasks.map((task, index) => (
-          <div data-testid="tasksList" key={task.id}>
+        {tasks.map((task) => (
+          <div className="tasksList" key={task.id}>
             <p>{task.task}</p>
             <p>{task.completed}</p>
+            <button
+              type="button"
+              className="deleteTask"
+              onClick={() => handleDelete(task.id)}
+            >
+              Delete
+            </button>
           </div>
         ))}
       </section>
