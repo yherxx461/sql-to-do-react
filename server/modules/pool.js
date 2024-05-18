0de7bca0fa8476fcaa3bfbd04a30228051f2a0be
Ying Her <yher.prime@gmail.com>
2024-05-18 12:40:01 -0500

    updated database name

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -3,7 +3,7 @@ const pg = require('pg');
 const pool = new pg.Pool({
   host: 'localhost',
   port: 5432,
-  database: 'weekend-to-do-app',
+  database: 'weekend_to_do_app',
 });
 
 module.exports = pool;
