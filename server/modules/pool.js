eb04f5ca124018e3fad329c37ecea76b8584d273
Ying Her <yher.prime@gmail.com>
2024-05-18 12:19:10 -0500

    update database

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,9 +1,9 @@
 const pg = require('pg');
 
 const pool = new pg.Pool({
-    host: 'localhost',
-    port: 5432,
-    database: 'weekend-to-do-app', 
+  host: 'localhost',
+  port: 5432,
+  database: 'prime-to-do-app',
 });
 
 module.exports = pool;
