3dd2cd8d5566161287d382b8e2b440c85d5d0377
Ying Her <yher.prime@gmail.com>
2024-05-18 12:43:24 -0500

    updated database to reflect routing

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -3,7 +3,7 @@ const pg = require('pg');
 const pool = new pg.Pool({
   host: 'localhost',
   port: 5432,
-  database: 'weekend_to_do_app',
+  database: 'weekend_to_do_app_xznk',
 });
 
 module.exports = pool;
